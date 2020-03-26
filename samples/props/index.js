7a655e9cb563a548d7ed7cbdeacb4f95a84ce905
Marc Espín Sanz <mespinsanz@gmail.com>
2020-03-26 10:04:13 +0100

    :sparkles: You can now set a default value when defining a prop

--- samples/props/index.js
+++ samples/props/index.js
@@ -5,59 +5,56 @@ const myState = new puffin.state({
 })
 
 myState.on('restarted',a=>{
-  myState.data.count = a
+  myState.data.count = 0
 })
 
 const firstComponent = puffin.element(
-  `
-     <div>
-        <p>Current: {{count}} and {{count}} and... finally {{count}} 😂</p>
-     </div>
-  `,
-  {
-    events:{
-      mounted(target){
-        target.props.count = myState.data.count
-        myState.changed(function(data){ //Update local state if global is updated
-          target.props.count = myState.data.count
-        })
-      }
-    },
-    props:["count"]
-  }
+	`
+	<div>
+		<p>Current: {{count}} and {{count}} and... finally {{count}} 😂</p>
+	</div>
+	`,
+	{
+		props:[{
+			name:'count',
+			default:0
+		}]
+	}
 );
 
 const secondComponent = puffin.element(
-  `
-     <div acount="{{count}}"> 
-        <firstComponent/>
-        <button click="$add">Add 1 to: {{count}}</button>
-        <button click="$restart">Restart</button>
-     </div>
-  `,
-  {
-    events:{
-      mounted(target){
-        target.props.count = myState.data.count
-
-        myState.changed(function(data){ //Update local state if global is updated
-          target.props.count = myState.data.count
-        })
-      }
-    },
-    methods:{
-      add(){
-        myState.data.count++ //Updates the global state
-      },
-      restart(){
-        myState.emit('restarted',0)
-      }
-    },
-    props:["count"],
-    components:{
-      firstComponent
-    }
-  }
+	`
+	<div count="{{count}}"> 
+		<firstComponent/>
+		<button click="$add">Add 1 to: {{count}}</button>
+		<button click="$restart">Restart</button>
+	</div>
+	`,
+	{
+		events:{
+			mounted(target){
+				myState.changed(function(data){ //Update local state if global is updated
+					target.props.count = myState.data.count
+				})
+				console.log(myState.data.count)
+			}
+		},
+		methods:{
+			add(){
+				myState.data.count++ //Updates the global state
+			},
+			restart(){
+				myState.emit('restarted',0)
+			}
+		},
+		props:[{
+			name:'count',
+			default:0
+		}],
+		components:{
+			firstComponent
+		}
+	}
 );
 
 
